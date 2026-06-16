d0f9c6b37966515db1e0c93835d289f47c520a05
caomihquan <71248793+caomihquan@users.noreply.github.com>
2026-06-16 10:31:02 -0400

    Update VocabularyController.cs

--- Controllers/VocabularyController.cs
+++ Controllers/VocabularyController.cs
@@ -14,7 +14,7 @@ namespace EnglishVoNo.Controllers
         [HttpGet("get")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
